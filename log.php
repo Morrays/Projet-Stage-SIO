21d7fc15df67b00f600d42bfe038121959063818
Kriiox <kriiox25@gmail.com>
2019-12-18 16:02:17 +0100

    Modification Login/Register
    Amélioration grapgique du formulaire de connexion et de création de compte

--- log.php
+++ log.php
@@ -4,108 +4,73 @@ if (ISSET ($_REQUEST["erreur"])){
     echo $_REQUEST["erreur"];
 }
 include 'connexion.php';
-/* * **********************************************************
- * Definition des constantes / tableaux et variables
- * *********************************************************** */
 
-// Constantes
-define('TARGET', 'images/');    // Repertoire cible
-define('MAX_SIZE', 100000);    // Taille max en octets du fichier
-define('WIDTH_MAX', 800);    // Largeur max de l'image en pixels
-define('HEIGHT_MAX', 800);    // Hauteur max de l'image en pixels
-// Tableaux de donnees
-$tabExt = array('jpg', 'gif', 'png', 'jpeg');    // Extensions autorisees
-$infosImg = array();
-
-// Variables
-$extension = '';
-$message = '';
-$nomImage = '';
-
-/* * **********************************************************
- * Creation du repertoire cible si inexistant
- * *********************************************************** */
-if (!is_dir(TARGET)) {
-    if (!mkdir(TARGET, 0755)) {
-        exit('Erreur : le répertoire cible ne peut-être créé ! Vérifiez que vous diposiez des droits suffisants pour le faire ou créez le manuellement !');
-    }
-}
-
-/* * **********************************************************
- * Script d'upload
- * *********************************************************** */
-if (!empty($_REQUEST)) {
-    // On verifie si le champ est rempli
-    if (!empty($_FILES['fichier']['name'])) {
-        // Recuperation de l'extension du fichier
-        $extension = pathinfo($_FILES['fichier']['name'], PATHINFO_EXTENSION);
-
-        // On verifie l'extension du fichier
-        if (in_array(strtolower($extension), $tabExt)) {
-            // On recupere les dimensions du fichier
-            $infosImg = getimagesize($_FILES['fichier']['tmp_name']);
-
-            // On verifie le type de l'image
-            if ($infosImg[2] >= 1 && $infosImg[2] <= 14) {
-                // On verifie les dimensions et taille de l'image
-                if (($infosImg[0] <= WIDTH_MAX) && ($infosImg[1] <= HEIGHT_MAX) && (filesize($_FILES['fichier']['tmp_name']) <= MAX_SIZE)) {
-                    // Parcours du tableau d'erreurs
-                    if (isset($_FILES['fichier']['error']) && UPLOAD_ERR_OK === $_FILES['fichier']['error']) {
-                        // On renomme le fichier
-//            $nomImage = md5(uniqid()) .'.'. $extension;
-                        $nomImage = $_FILES['fichier']['name'];
-                        $_SESSION ['nomImage'] = $nomImage;
-                        //var_dump($_FILES['fichier']);
-                        // Si c'est OK, on teste l'upload
-                        if (move_uploaded_file($_FILES['fichier']['tmp_name'], TARGET . $nomImage)) {
-                            $message = 'Upload réussi !';
-                        } else {
-                            // Sinon on affiche une erreur systeme
-                            $message = 'Problème lors de l\'upload !';
-                        }
-                    } else {
-                        $message = 'Une erreur interne a empêché l\'uplaod de l\'image';
-                    }
-                } else {
-                    // Sinon erreur sur les dimensions et taille de l'image
-                    $message = 'Erreur dans les dimensions de l\'image !';
-                }
-            } else {
-                // Sinon erreur sur le type de l'image
-                $message = 'Le fichier à uploader n\'est pas une image !';
-            }
-        } else {
-            // Sinon on affiche une erreur pour l'extension
-            $message = 'L\'extension du fichier est incorrecte !';
-        }
-    } else {
-        // Sinon on affiche une erreur pour le champ vide
-        $message = 'Veuillez remplir le formulaire svp !';
-    }
-}
 ?>
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr">
-    <head>
-        <title>Recherche de stage</title>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous"/>
-        <link rel="stylesheet" type="text/css" href="style.css">
-            <link href="https://fonts.googleapis.com/css?family=Lato:400,700&subset=latin-ext" rel="stylesheet"/>
-            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-            <meta charset="utf-8"/>
-            <script type="text/javascript" src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
-            <script type="text/javascript" src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
-            <link rel="stylesheet" type="text/css" href="http://code.jquery.com/ui/1.7.3/themes/base/jquery-ui.css"/>
-            <link rel="stylesheet" type="text/css" href="https://code.jquery.com/ui/1.7.3/themes/base/jquery-ui.css"/>
-            <script src="main.js" type="text/javascript"></script>
-    </head>
-    <header>
-        <?php
+
+<head>
+    <title>Recherche de stage</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"
+        integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous" />
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,700&subset=latin-ext" rel="stylesheet" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <meta charset="utf-8" />
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+    <script type="text/javascript" src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="http://code.jquery.com/ui/1.7.3/themes/base/jquery-ui.css" />
+    <link rel="stylesheet" type="text/css" href="https://code.jquery.com/ui/1.7.3/themes/base/jquery-ui.css" />
+    <script src="main.js" type="text/javascript"></script>
+</head>
+<header>
+    <?php
         include 'barrenav.php';
+<<<<<<< HEAD
+    ?>
+</header>
+
+<body>
+    <!-- Modal // ajout  BootStrap + JS -->
+    <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog"
+        aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <p>Vous devez être connecté pour accéder à la recherche</p>
+                    <img src="images/tenor.gif" alt="Tenor qui branche un fil" />
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-primary" data-dismiss="modal">Fermer</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Fin modal -->
+    <br><br><br>
+    <div id="conteneurlog">
+        <div id="login" class="">
+            <form action="connexionetudiant.php" method="POST" class="form-signin" id="login">
+                <h1 class="h3 mb-3 font-weight-normal">Identification</h1>
+                <label for="inputNom" class="sr-only">Nom</label>
+                <input type="text" id="inputNom" class="form-control" placeholder="Nom" name="nom" required autofocus>
+                <label for="inputPassword" class="sr-only">Password</label>
+                <input type="password" id="inputPassword" class="form-control" placeholder="Password" name="mdp"
+                    required>
+                <button class="btn btn-lg btn-primary btn-block" type="submit">Connexion</button>
+                <a onclick="registerForm()">Créer un compte</a>
+            </form>
+        </div>
+=======
         ?>
     </header>
     <body>
-        <!-- Modal // ajout  BootStrap + JS -->   
+        <!-- Modal // ajout  BootStrap + JS -->
         <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
             <div class="modal-dialog modal-dialog-centered" role="document">
                 <div class="modal-content">
@@ -132,7 +97,7 @@ if (!empty($_REQUEST)) {
                             <a class="hiddenanchor" id="tologin"></a>
                             <div id="wrapper">
                                 <div id="login" class="animate form">
-                                    <!-- <form  action="connexionetudiant.php" method="POST"> 
+                                    <!-- <form  action="connexionetudiant.php" method="POST">
                                         champs pour se connecter avec son compte, appelle laconnexion.php
                                         <br><br><br><br>
                                         <h1>Se connecter</h1>
@@ -144,30 +109,95 @@ if (!empty($_REQUEST)) {
                                                 Mdp : <input type="password" name="mdp" value="" /><br />
                                             </p>
                                             <p>
-                                                <input class="button" type="submit" value="Connexion" /> 
+                                                <input class="button" type="submit" value="Connexion" />
                                             </p>
                                     </form> -->
+>>>>>>> d249f2501a39da8448053a267dd116ced77c01a9
 
-                                    <form action="connexionetudiant.php" method="POST" class="form-signin">
-                                    <h1 class="h3 mb-3 font-weight-normal">Identification</h1>
-                                    <label for="inputNom" class="sr-only">Nom</label>
-                                    <input type="text" id="inputNom" class="form-control" placeholder="Nom" name="nom" required autofocus>
-                                    <label for="inputPassword" class="sr-only">Password</label>
-                                    <input type="password" id="inputPassword" class="form-control" placeholder="Password" name="mdp" required>
-                                    <div class="checkbox mb-3">
-                                        <label>
-                                        <input type="checkbox" value="remember-me"> Remember me
-                                        </label>
-                                    </div>
-                                    <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-                                    </form>
+        <div class="hidden" id="register">
+            <form action="newetudiant.php" method="GET" class="form-signin">
 
+<<<<<<< HEAD
+                <h1 class="h3 mb-3 font-weight-normal">Créer un compte</h1>
+                <label for="inputNom" class="sr-only">Nom</label>
+                <input type="text" id="inputNom" class="form-control" placeholder="Nom" name="nom" required autofocus>
+                <label for="inputPrenom" class="sr-only">Prenom</label>
+                <input type="text" id="inputPrenom" class="form-control" placeholder="Prenom" name="prenom" required
+                    autofocus>
+                <label for="inputPassword" class="sr-only">Password</label>
+                <input type="password" id="inputPassword" class="form-control" placeholder="Password" name="mdp"
+                    required>
+                <label for="inputMail" class="sr-only">Mail</label>
+                <input type="email" id="inputMail" class="form-control" placeholder="name@example.com" name="email"
+                    required>
+
+                <?php
+                $sql = "SELECT * FROM sta_classe WHERE idclasse < 3";
+                $q = $connection->query($sql);
+                echo "<div class='form-group'>";
+                echo "<label for='selectClasse'>Classe</label>";
+                echo "<select name='classe' class='form-control' id='selectClasse'>";                                            
+                while ($ligne = $q->fetch()) {
+                    if ($row['libelle_classe'] == $ligne[0])
+                        echo "<option value=" . $ligne[0] . " selected='selected'>" . $ligne[1] . "</option>";
+                    else
+                        echo "<option value=" . $ligne[0] . ">" . $ligne[1] . "</option>";
+                }
+                echo "</select>";
+                echo "</div>";
+                ?>
+
+                <?php
+                $sql = "SELECT * FROM sta_promotion WHERE id_promotion > 1";
+                $q = $connection->query($sql);
+                echo "<div class='form-group'>";
+                echo "<label for='selectPromotion'>Promotion</label>";
+                echo "<select name='promotion' class='form-control' id='selectPromotion'>"; 
+                while ($ligne = $q->fetch()) {
+                    if ($row['libelle_promotion'] == $ligne[0])
+                        echo "<option value=" . $ligne[0] . " selected='selected'>" . $ligne[1] . "</option>";
+                    else
+                        echo "<option value=" . $ligne[0] . ">" . $ligne[1] . "</option>";
+                }
+                echo "</select>";                                            
+                echo "</div>";
+                ?>
+                <label for="fichier_a_uploader" title="Recherchez le fichier à uploader !">Photo :</label>
+                <input type="hidden" name="MAX_FILE_SIZE" value="<?php echo MAX_SIZE; ?>" />
+                <input name="fichier" type="file" id="fichier_a_uploader" />
+                <input type="submit" name="submit" value="Uploader" />
+
+
+                <button class="btn btn-lg btn-primary btn-block" type="submit">Créer</button>
+                <a onclick="loginForm()">Connexion</a>
+            </form>
+        </div>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous" />
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"
+        integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous" />
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"
+        integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous" />
+    </script>
+    <script type="text/javascript">
+    var op = <?PHP echo(!empty($_GET['err']) ? json_encode($_GET['err']) : '""'); ?> ;
+    if (op == 1) {
+        $('#exampleModalCenter').modal('show');
+    }
+    </script>
+</body>
+
+</html>
+=======
                                 </div>
                             </div>
                         </div>
-                    <div id="register" class="animate form">  
+                    <div id="register" class="animate form">
                         <!--//champs pour se créer un compte et l'ajouter a la bdd-->
-                        <h1>Créer un compte</h1> 
+                        <h1>Créer un compte</h1>
                         <br>
                             <form enctype="multipart/form-data" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
                                 <fieldset>
@@ -179,15 +209,15 @@ if (!empty($_REQUEST)) {
                                     </p>
                                 </fieldset>
                             </form>
-                            <form  action="newetudiant.php" method="GET"> 
-                                <p> 
+                            <form  action="newetudiant.php" method="GET">
+                                <p>
                                     Nom : <input type="text" name="nom" value="" /><br />
                                 </p>
-                                <p> 
+                                <p>
                                     Prénom : <input type="text" name="prenom" value="" /><br />
                                 </p>
                                 <p>
-                                    Classe :  
+                                    Classe :
                                     <?php
                                     $sql = "SELECT * FROM sta_classe WHERE idclasse < 3";
                                     $q = $connection->query($sql);
@@ -199,10 +229,25 @@ if (!empty($_REQUEST)) {
                                             echo "<option value=" . $ligne[0] . ">" . $ligne[1] . "</option>";
                                     }
                                     echo "</select>";
-                                    ?> 
+                                    ?>
                                 </p>
                                 <p>
-                                    Promotion :  
+                                    Option :
+                                    <?php
+                                    $sql = "SELECT * FROM sta_option WHERE idoption <3";
+                                    $q = $connection->query($sql);
+                                    echo "<select name = 'option' >";
+                                    while ($ligne = $q->fetch()) {
+                                        if ($row['libelle_option'] == $ligne[0])
+                                            echo "<option value=" . $ligne[0] . " selected='selected'>" . $ligne[1] . "</option>";
+                                        else
+                                            echo "<option value=" . $ligne[0] . ">" . $ligne[1] . "</option>";
+                                    }
+                                    echo "</select>";
+                                    ?>
+                                </p>
+                                <p>
+                                    Promotion :
                                     <?php
                                     $sql = "SELECT * FROM sta_promotion WHERE id_promotion > 1";
                                     $q = $connection->query($sql);
@@ -214,7 +259,7 @@ if (!empty($_REQUEST)) {
                                             echo "<option value=" . $ligne[0] . ">" . $ligne[1] . "</option>";
                                     }
                                     echo "</select>";
-                                    ?> 
+                                    ?>
                                 </p>
                                 <p>
                                     Mail : <input type="text" name="email" value="" /><br />
@@ -238,3 +283,4 @@ if (!empty($_REQUEST)) {
                     </script>
     </body>
 </html>
+>>>>>>> d249f2501a39da8448053a267dd116ced77c01a9
--- newetudiant.php
+++ newetudiant.php
@@ -11,12 +11,88 @@ if (!empty($_REQUEST['nom']) AND !empty($_REQUEST['prenom']) AND !empty($_REQUES
     else $photo = "";
     $email = htmlentities($_REQUEST["email"]);
     $mdp = htmlentities($_REQUEST["mdp"]);
+    
+    /* * **********************************************************
+ * Definition des constantes / tableaux et variables
+ * *********************************************************** */
 
-    //hash des mdp en SHA256
-    $mdpH = password_hash($mdp, MHASH_SHA256);
+// Constantes
+define('TARGET', 'images/');    // Repertoire cible
+define('MAX_SIZE', 100000);    // Taille max en octets du fichier
+define('WIDTH_MAX', 800);    // Largeur max de l'image en pixels
+define('HEIGHT_MAX', 800);    // Hauteur max de l'image en pixels
+// Tableaux de donnees
+$tabExt = array('jpg', 'gif', 'png', 'jpeg');    // Extensions autorisees
+$infosImg = array();
+
+// Variables
+$extension = '';
+$message = '';
+$nomImage = '';
+
+/* * **********************************************************
+ * Creation du repertoire cible si inexistant
+ * *********************************************************** */
+if (!is_dir(TARGET)) {
+    if (!mkdir(TARGET, 0755)) {
+        exit('Erreur : le répertoire cible ne peut-être créé ! Vérifiez que vous diposiez des droits suffisants pour le faire ou créez le manuellement !');
+    }
+}
+
+/* * **********************************************************
+ * Script d'upload
+ * *********************************************************** */
+if (!empty($_REQUEST)) {
+    // On verifie si le champ est rempli
+    if (!empty($_FILES['fichier']['name'])) {
+        // Recuperation de l'extension du fichier
+        $extension = pathinfo($_FILES['fichier']['name'], PATHINFO_EXTENSION);
+
+        // On verifie l'extension du fichier
+        if (in_array(strtolower($extension), $tabExt)) {
+            // On recupere les dimensions du fichier
+            $infosImg = getimagesize($_FILES['fichier']['tmp_name']);
+
+            // On verifie le type de l'image
+            if ($infosImg[2] >= 1 && $infosImg[2] <= 14) {
+                // On verifie les dimensions et taille de l'image
+                if (($infosImg[0] <= WIDTH_MAX) && ($infosImg[1] <= HEIGHT_MAX) && (filesize($_FILES['fichier']['tmp_name']) <= MAX_SIZE)) {
+                    // Parcours du tableau d'erreurs
+                    if (isset($_FILES['fichier']['error']) && UPLOAD_ERR_OK === $_FILES['fichier']['error']) {
+                        // On renomme le fichier
+//            $nomImage = md5(uniqid()) .'.'. $extension;
+                        $nomImage = $_FILES['fichier']['name'];
+                        $_SESSION ['nomImage'] = $nomImage;
+                        //var_dump($_FILES['fichier']);
+                        // Si c'est OK, on teste l'upload
+                        if (move_uploaded_file($_FILES['fichier']['tmp_name'], TARGET . $nomImage)) {
+                            $message = 'Upload réussi !';
+                        } else {
+                            // Sinon on affiche une erreur systeme
+                            $message = 'Problème lors de l\'upload !';
+                        }
+                    } else {
+                        $message = 'Une erreur interne a empêché l\'uplaod de l\'image';
+                    }
+                } else {
+                    // Sinon erreur sur les dimensions et taille de l'image
+                    $message = 'Erreur dans les dimensions de l\'image !';
+                }
+            } else {
+                // Sinon erreur sur le type de l'image
+                $message = 'Le fichier à uploader n\'est pas une image !';
+            }
+        } else {
+            // Sinon on affiche une erreur pour l'extension
+            $message = 'L\'extension du fichier est incorrecte !';
+        }
+    } else {
+        // Sinon on affiche une erreur pour le champ vide
+        $message = 'Veuillez remplir le formulaire svp !';
+    }
+}
     
-    
-    ?>
+?>
     <!DOCTYPE html>
     <html>
         <!--//permet d'enregistrer les infos rentrer lors de l'inscription d'un nouveau client-->
@@ -41,7 +117,7 @@ if (!empty($_REQUEST['nom']) AND !empty($_REQUEST['prenom']) AND !empty($_REQUES
                     </br>
                     Votre inscription a bien été effectuée.
                 <p>
-                    <input class="button" type="submit" onclick="window.location.href = 'index.php'" value="Retour à l'accueil"/>
+                    <input class="btn btn-primary" type="submit" onclick="window.location.href = 'index.php'" value="Retour à l'accueil"/>
                 </p>                
                 <?php 
                 $req = $connection->prepare('INSERT INTO sta_etudiant(nom, prenom, idclasse, idpromotion, email, photo, mdp) VALUES(:nom, :prenom, :classe, :promotion, :mail, :photo, :mdp)');
@@ -52,7 +128,7 @@ if (!empty($_REQUEST['nom']) AND !empty($_REQUEST['prenom']) AND !empty($_REQUES
                     'promotion' => $promotion,
                     'mail' => $email,
                     'photo' => $photo,
-                    'mdp' => $mdpH,
+                    'mdp' => $mdp,
                 ));
             }
 else {
