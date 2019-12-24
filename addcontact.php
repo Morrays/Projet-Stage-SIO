82b97b4cc4c364f4a4bac368c016da9e6a181af5
Kriiox <kriiox25@gmail.com>
2019-12-24 17:40:09 +0100

    update graphisue addcontact et addentreprise
    update graphique a l'aide de bootstrap

--- addcontact.php
+++ addcontact.php
@@ -1,6 +1,5 @@
 <?php
-session_start();
-include('connexion.php');
+include "header.php";
 
 if (isset($_POST['submit'])) {
 
@@ -21,64 +20,54 @@ if (isset($_POST['submit'])) {
     }
 }
 ?>
-
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Ajouter contact</title>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-        <meta charset="utf-8">
-        <script type="text/javascript" src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
-        <script type="text/javascript" src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
-        <link rel="stylesheet" type="text/css" href="http://code.jquery.com/ui/1.7.3/themes/base/jquery-ui.css">
-        <link rel="stylesheet" type="text/css" href="https://code.jquery.com/ui/1.7.3/themes/base/jquery-ui.css">
-        <script src="main.js" type="text/javascript"></script>
-    </head>
-    <body style="margin: 200px;">
-        <h2>Nouveau contact</h2>
+<br>
+<h2>Ajouter un contact</h2>
+<?php
+if (isset($erreur)) {
+    echo $erreur;
+}
+?>
+<!--	//permet de rentrer de nouvelles infos clients et de les enregistrer dans la bdd-->
+<div class="d-inline-flex p-2">
+<form method="POST" action="">
+    <div class="form-group">
+        <label for="">Nom</label>
+        <input type="text" class="form-control" id="example" name="nom" placeholder="Nom">
+    </div>
+    <div class="form-group">
+        <label for="">Prenom</label>
+        <input type="text" class="form-control" id="example" name="prenom" placeholder="Prenom">
+    </div>
+    <div class="form-group">
+        <label for="">Téléphone</label>
+        <input type="text" class="form-control" id="example" name="tel" placeholder="Telephone">
+    </div>
+    <div class="form-group">
+        <label for="">Mail</label>
+        <input type="text" class="form-control" id="example" name="mail" placeholder="Mail">
+    </div>
+    <div class="form-group">
+        <label for="">Rôle</label>
+        <input type="text" class="form-control" id="example" name="role" placeholder="Role">
+    </div>
+    <div class="form-group">
+        <label for="">Service</label>
+        <input type="text" class="form-control" id="example" name="service" placeholder="Service">
+    </div>
+    <div class="form-group">
+        <label for="">Entreprise</label>
+        <br>
         <?php
-        if (isset($erreur)) {
-            echo $erreur;
-        }
+        $sql = "SELECT * FROM sta_entreprise WHERE SIRET = '".$_REQUEST['SIRET']."'";
+        $q = $connection->query($sql);
+        $ligne = $q->fetch();
+        echo $ligne ['nom'];
         ?>
-        <!--	//permet de rentrer de nouvelles infos clients et de les enregistrer dans la bdd-->
-        <form method="POST" action="">
-            <div class="form-group">
-                <label for="">Nom</label>
-                <input type="text" class="form-control" id="example" name="nom" placeholder="Nom">
-            </div>
-            <div class="form-group">
-                <label for="">Prenom</label>
-                <input type="text" class="form-control" id="example" name="prenom" placeholder="Prenom">
-            </div>
-            <div class="form-group">
-                <label for="">Téléphone</label>
-                <input type="text" class="form-control" id="example" name="tel" placeholder="Telephone">
-            </div>
-            <div class="form-group">
-                <label for="">Mail</label>
-                <input type="text" class="form-control" id="example" name="mail" placeholder="Mail">
-            </div>
-            <div class="form-group">
-                <label for="">Rôle</label>
-                <input type="text" class="form-control" id="example" name="role" placeholder="Role">
-            </div>
-            <div class="form-group">
-                <label for="">Service</label>
-                <input type="text" class="form-control" id="example" name="service" placeholder="Service">
-            </div>
-            <div class="form-group">
-                <label for="">Entreprise</label>
-                <br>
-                <?php
-                $sql = "SELECT * FROM sta_entreprise WHERE SIRET = '".$_REQUEST['SIRET']."'";
-                $q = $connection->query($sql);
-                $ligne = $q->fetch();
-                echo $ligne ['nom'];
-                ?>
-            </div>
-            <button type="submit" class="btn btn-primary" name="submit">Ajouter à la base de données</button>
-            <a class="btn btn-success" href="addDemande.php?">Retour</a>
-        </form>
-    </body>
-</html>
+    </div>
+    <button type="submit" class="btn btn-primary" name="submit"><i
+                    class="fas fa-check-square"></i> Valider</button>
+    <a class="btn btn-danger" href="addDemande.php?"><i class="fas fa-backspace"></i> Retour</a>
+</form>
+</div>
+
+<?php include "footer.php"; ?>
--- addentreprise.php
+++ addentreprise.php
@@ -1,6 +1,5 @@
 <?php
-session_start();
-include('connexion.php');
+include "header.php";
 
 if (isset($_REQUEST['submit'])) {
 
@@ -20,33 +19,24 @@ if (isset($_REQUEST['submit'])) {
     //header('location: addDemande.php');
 }
 ?>
-
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Ajouter une entreprise</title>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-        <meta charset="utf-8">
-        <script type="text/javascript" src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
-        <script type="text/javascript" src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
-        <link rel="stylesheet" type="text/css" href="http://code.jquery.com/ui/1.7.3/themes/base/jquery-ui.css">
-        <link rel="stylesheet" type="text/css" href="https://code.jquery.com/ui/1.7.3/themes/base/jquery-ui.css">
-        <script src="main.js" type="text/javascript"></script>
-    </head>
-    <body style="margin: 200px;">
-        <h2>Nouvelle entreprise</h2>
-        <a href="https://www.manageo.fr/" target="_blank">Trouver le SIRET et le code NAF de l'entreprise</a>
-        <br>
-        <a href="https://blog.easyfichiers.com/wp-content/uploads/2014/08/Liste-code-naf-ape.pdf" target="_blank">Trouver la division NAF de l'entreprise</a>
-        <br>
-        <a href="https://public.opendatasoft.com/explore/dataset/correspondance-code-insee-code-postal/table/" target="_blank">Trouver le code postal unique</a>
-        <br><br>
-        <?php
+<div class="container">
+    <br>
+    <h2>Ajouter un entreprise</h2>
+    <a href="https://www.manageo.fr/" target="_blank">Trouver le SIRET et le code NAF de l'entreprise</a>
+    <br>
+    <a href="https://blog.easyfichiers.com/wp-content/uploads/2014/08/Liste-code-naf-ape.pdf" target="_blank">Trouver la
+        division NAF de l'entreprise</a>
+    <br>
+    <a href="https://public.opendatasoft.com/explore/dataset/correspondance-code-insee-code-postal/table/"
+        target="_blank">Trouver le code postal unique</a>
+    <br><br>
+    <?php
         if (isset($erreur)) {
             echo $erreur;
         }
         ?>
-        <!--	//permet de rentrer de nouvelles infos clients et de les enregistrer dans la bdd-->
+    <!--	//permet de rentrer de nouvelles infos clients et de les enregistrer dans la bdd-->
+    <div class="d-inline-flex p-2">
         <form method="GET" action="">
             <div class="form-group">
                 <label for="">SIRET</label>
@@ -57,20 +47,19 @@ if (isset($_REQUEST['submit'])) {
                 <input type="text" class="form-control" id="example" name="nom" placeholder="Nom">
             </div>
             <div class="form-group">
-                <label for="">Division NAF</label>
+                <label for="selectNaf">Division NAF</label>
                 <br>
                 <?php
-                $sql = "SELECT * FROM sta_naf";
-                $q = $connection->query($sql);
-                echo "<select name = 'code_NAF' >";
-                while ($ligne = $q->fetch()) {
-                    if ($row['code_NAF'] == $ligne[0])
-                        echo "<option value=" . $ligne[0] . " selected='selected'>" . $ligne[1] . "</option>";
-                    else
-                        echo "<option value=" . $ligne[0] . ">" . $ligne[1] . "</option>";
-                }
-                echo "</select>";
-                ?> 
+        $sql = "SELECT * FROM sta_naf order by libelle_NAF asc";
+        $q = $connection->query($sql); ?>
+                <select name='code_NAF' class="form-control" id="selectNaf">
+                    <?php while ($ligne = $q->fetch()) {
+            if ($row['code_NAF'] == $ligne[0])
+                echo "<option value=" . $ligne[0] . " selected='selected'>" . $ligne[1] . "</option>";
+            else
+                echo "<option value=" . $ligne[0] . ">" . $ligne[1] . "</option>";
+        } ?>
+                </select>
             </div>
             <div class="form-group">
                 <label for="">Téléphone</label>
@@ -84,10 +73,13 @@ if (isset($_REQUEST['submit'])) {
                 <label for="">Ville</label>
                 <br>
                 <input name="cpville" id="cpville" type="hidden" placeholder="Code Postal">
-                <input name="ville" id="ville" type="text" placeholder="Ville">
+                <input name="ville" class="form-control" id="ville" type="text" placeholder="Ville">
             </div>
-            <button type="submit" class="btn btn-primary" name="submit">Ajouter à la base de données</button>
-            <a class="btn btn-success" href="addDemande.php?">Retour</a>
+            <button type="submit" class="btn btn-primary" name="submit"><i
+                    class="fas fa-check-square"></i> Valider</button>
+            <a class="btn btn-danger" href="addDemande.php?"><i class="fas fa-backspace"></i> Retour</a>
         </form>
-    </body>
-</html>
+    </div>
+</div>
+
+<?php include "footer.php"; ?>
