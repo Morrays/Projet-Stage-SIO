9a7e0b8c9a018fa4d168b689a4a717690eb249c5
Kriiox <kriiox25@gmail.com>
2020-01-15 14:08:06 +0100

    modif select stage.php

--- stage.php
+++ stage.php
@@ -1,5 +1,14 @@
 <?php
 include 'header.php';
+
+if (isset($_GET['idpromotion']) && (isset($_GET['idetat']))){
+    $idpromo = $_GET['idpromotion'];
+    $idetat = $_GET['idetat'];
+    $sql = 'SELECT ent.nom AS nomEnt, etu.nom, d.date_demande, eta.libelle_etat, d.refus, p.date_debut, p.date_fin, prom.libelle_promotion FROM sta_demande d, sta_etudiant etu, sta_entreprise ent, sta_etat eta, sta_periode p, sta_promotion prom WHERE prom.id_promotion=etu.idpromotion and etu.idetudiant = d.idetudiant AND ent.SIRET = d.SIRET AND d.idetat =eta.idetat AND p.idperiode = d.idperiode and prom.id_promotion='.$idpromo.' AND eta.idetat='.$idetat.' ORDER BY d.date_demande';
+} else {
+    $sql = 'SELECT ent.nom AS nomEnt, etu.nom, d.date_demande, eta.libelle_etat, d.refus, p.date_debut, p.date_fin FROM sta_demande d, sta_etudiant etu, sta_entreprise ent, sta_etat eta, sta_periode p WHERE etu.idetudiant = d.idetudiant AND ent.SIRET = d.SIRET AND d.idetat =eta.idetat AND p.idperiode = d.idperiode ORDER BY d.date_demande ';
+}
+$reponse1 = $connection->query($sql);
 ?>
 <div class="container">
     <br><br><br>
@@ -55,7 +64,7 @@ include 'header.php';
 </div>
 
 <div class="row d-flex justify-content-center"">
-    <form>
+    <form action="" method="get">
 
         <div class="form-group">
             <label for="selectPromotion">Sélectionner une promotion :</label>
@@ -64,14 +73,10 @@ include 'header.php';
                 $sql = "SELECT * FROM sta_promotion WHERE id_promotion > 1";
                 $q = $connection->query($sql); ?>
             <select class="form-control" name='idpromotion' id="selectPromotion">
-                <?php while ($ligne = $q->fetch()) {
-                    if ($row['idpromotion'] == $ligne[0])
-                        echo "<option value=" . $ligne[0] . " selected='selected'>" . $ligne[1] . "</option>";
-                    else
-                        echo "<option value=" . $ligne[0] . ">" . $ligne[1] . "</option>";
-                }
-                echo "</select>";
-                ?>
+                <?php while ($ligne = $q->fetch()) { ?>
+                        <option value="<?php echo $ligne[0] ?>" <?php if(isset($_GET['idpromotion']) && $ligne[0] == $_GET['idpromotion']){echo 'selected'; }?>><?php echo $ligne[1] ?></option>
+                    <?php } ?>
+            </select>
         </div>
 
         <div class="form-group">
@@ -82,14 +87,13 @@ include 'header.php';
                 $q = $connection->query($sql);
                 ?>
             <select class="form-control" name='idetat' id="selectEtat">
-                <?php while ($ligne = $q->fetch()) {
-                    if ($row['idetat'] == $ligne[0])
-                        echo "<option value=" . $ligne[0] . " selected='selected'>" . $ligne[1] . "</option>";
-                    else
-                        echo "<option value=" . $ligne[0] . ">" . $ligne[1] . "</option>";
-                } ?>
-            </select>
+                <?php while ($ligne = $q->fetch()) { ?>
+                    <option value="<?php echo $ligne[0] ?>" <?php if(isset($_GET['idetat']) && $ligne[0] == $_GET['idetat']){echo 'selected'; }?>><?php echo $ligne[1] ?></option>
+                <?php } ?>
+                </select>
         </div>
+
+        <input type="submit" value="Rechercher" class="btn btn-primary">
 
     </form>
 </div>
@@ -107,7 +111,7 @@ include 'header.php';
         <br />
         <div class="row">
             <tbody>
-                <table class="table table-bordered">
+                <table class="table table-striped">
                     <thead>
                         <tr>
                             <th data-column-id="id" data-type="numeric" data-identifier="true">Entreprise</th>
@@ -119,18 +123,9 @@ include 'header.php';
                     </thead>
                     <tbody>
                         <?php
-                        if (isset($_SESSION['nom'])) {
-                            if (isset($_GET["s"]) AND $_GET["s"] == "Rechercher") {
-                                $_GET["termeR"] = htmlspecialchars($_GET["termeR"]); //pour sécuriser le formulaire contre les failles html
-                                $terme = $_GET["termeR"];
-                                $terme = trim($terme); //pour supprimer les espaces dans la requête de l'internaute
-                                $terme = strip_tags($terme); //pour supprimer les balises html dans la requête
-                            }
-                            $reponse = $connection->query('SELECT ent.nom AS nomEnt, etu.nom, d.date_demande, eta.libelle_etat, d.refus, p.date_debut, p.date_fin '
-                                    . 'FROM sta_demande d, sta_etudiant etu, sta_entreprise ent, sta_etat eta, sta_periode p '
-                                    . 'WHERE etu.idetudiant = d.idetudiant AND ent.SIRET = d.SIRET AND d.idetat =eta.idetat AND p.idperiode = d.idperiode ORDER BY d.date_demande ');
+                        if (isset($_SESSION['nom'])) {                            
 
-                            while ($donnees = $reponse->fetch()) {
+                            while ($donnees = $reponse1->fetch()) {
                                 $don = '<tr>
                                <td>' . $donnees['nomEnt'] . '</td>
                                <td>' . $donnees['date_demande'] . '</td>
@@ -152,7 +147,7 @@ include 'header.php';
                                 <p></tr>'
                                 ;
                             }
-                            $reponse->closeCursor();
+                            $reponse1->closeCursor();
                             $_SESSION['deco'] = '1';
 //                                echo '<a class="btn btn-success" href="addDemande.php?">Ajouter une demande de stage</a>';
                             ?>
