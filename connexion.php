d4e5c9dd94bfd674a38a33035c62305c3a1391bf
Kriiox <kriiox25@gmail.com>
2020-01-22 13:40:34 +0100

    Update connexion.php

--- connexion.php
+++ connexion.php
@@ -1,9 +1,9 @@
 <?php
 // Connection au serveur
 try {
-    $dns = 'mysql:host=localhost;dbname=stpolsis45';
-    $utilisateur = 'root';
-    $motDePasse = '';
+    $dns = 'mysql:host=kevingosset.fr;dbname=kriiox_sio';
+    $utilisateur = 'kriiox_sio';
+    $motDePasse = 'sio1212';
     $connection = new PDO($dns, $utilisateur, $motDePasse);
     $connection->query("SET NAMES utf8");
 } catch (Exception $e) {
