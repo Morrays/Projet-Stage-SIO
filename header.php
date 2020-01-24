e2eb3063d1f31fe31580ce792fbf82d34188d4a2
Morrays <55636996+Morrays@users.noreply.github.com>
2020-01-24 13:15:18 +0100

    Revert "Modification header dashboard / ajout gestion entreprises"
    This reverts commit eadd8d65c42364c3274b2126998bc1c36ed9112f.

--- header.php
+++ header.php
@@ -1,116 +1,67 @@
 <?php
 session_start();
-include "../connexion.php";
-if ($_SESSION['idpromo'] != 1){
-    header("Location: ../index.php");
-}
-
-$sqlphoto = "SELECT photo FROM sta_etudiant WHERE idetudiant = " . $_SESSION['code'];
-$q = $connection->query($sqlphoto);
-$ligne = $q->fetch();
+include 'connexion.php';
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="fr">
 
 <head>
+    <title>Gestion des stages</title>
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Dashboard</title>
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="robots" content="all,follow">
-    <!-- Bootstrap CSS-->
-    <link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
-    <!-- Font Awesome CSS-->
-    <link rel="stylesheet" href="vendor/font-awesome/css/font-awesome.min.css">
-    <!-- Fontastic Custom icon font-->
-    <link rel="stylesheet" href="css/fontastic.css">
-    <!-- Google fonts - Roboto -->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700">
-    <!-- jQuery Circle-->
-    <link rel="stylesheet" href="css/grasp_mobile_progress_circle-1.0.0.min.css">
-    <!-- Custom Scrollbar-->
-    <link rel="stylesheet" href="vendor/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.css">
-    <!-- theme stylesheet-->
-    <link rel="stylesheet" href="css/style.default.css" id="theme-stylesheet">
-    <!-- Custom stylesheet - for your changes-->
-    <link rel="stylesheet" href="css/custom.css">
-    <!-- Favicon-->
-    <link rel="shortcut icon" href="img/favicon.ico">
-    <!-- Tweaks for older IEs-->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
+
+    <!-- POLICE ECRITURE -->
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,700&subset=latin-ext" rel="stylesheet">
+    <!--Jquery UI  -->
+    <link rel="stylesheet" href="jquery-ui-1.12.1/jquery-ui.min.css">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">      
+    <!-- PERSO -->
+    <link rel="stylesheet" type="text/css" href="asset/style.css"> 
+
 </head>
 
 <body>
-    <!-- Side Navbar -->
-    <nav class="side-navbar">
-        <div class="side-navbar-wrapper">
-            <!-- Sidebar Header    -->
-            <div class="sidenav-header d-flex align-items-center justify-content-center">
-                <!-- User Info-->
-                <div class="sidenav-header-inner text-center"><img src="../images/<?php echo $ligne['photo']?>"
-                        alt="person" class="img-fluid rounded-circle">
-                    <h2 class="h5"><?php echo $_SESSION['nom']." ".$_SESSION['prenom'] ?></h2><span>Admin</span>
-                </div>
-                <!-- Small Brand information, appears on minimized sidebar-->
-                <div class="sidenav-header-logo"><a href="index.html" class="brand-small text-center">
-                        <strong>B</strong><strong class="text-primary">D</strong></a></div>
-            </div>
-            <!-- Sidebar Navigation Menus-->
-            <div class="main-menu">
-                <h5 class="sidenav-heading">Main</h5>
-                <ul id="side-main-menu" class="side-menu list-unstyled">
-                    <li><a href="index.php"> <i class="icon-home"></i>DASHBOARD </a></li>
-                    <li><a href="gestionEleves.php"> <i class="icon-home"></i>GESTION ELEVES </a></li>
-                    <li><a href="gestionEntreprises.php"> <i class="icon-home"></i>GESTION ENTREPRISES </a></li>
-                    <li><a href="index.php"> <i class="icon-home"></i>PERIODES </a></li>
-                    <li><a href="index.php"> <i class="icon-home"></i>STATS </a></li>
-                </ul>
-            </div>
-            <!-- <div class="admin-menu">
-                <h5 class="sidenav-heading">Second menu</h5>
-                <ul id="side-admin-menu" class="side-menu list-unstyled">
-                    <li> <a href="#"> <i class="icon-screen"> </i>Demo</a></li>
-                </ul>
-            </div> -->
-        </div>
-    </nav>
-    <div class="page">
-        <!-- navbar-->
-        <header class="header">
-            <nav class="navbar">
-                <div class="container-fluid">
-                    <div class="navbar-holder d-flex align-items-center justify-content-between">
-                        <div class="navbar-header"><a id="toggle-btn" href="#" class="menu-btn"><i class="icon-bars">
-                                </i></a><a href="index.html" class="navbar-brand">
-                                <div class="brand-text d-none d-md-inline-block"><span>Bootstrap </span><strong
-                                        class="text-primary">Dashboard</strong></div>
-                            </a></div>
-                        <ul class="nav-menu list-unstyled d-flex flex-md-row align-items-md-center">
-                            <!-- Notifications dropdown-->
-                            <li class="nav-item dropdown"> <a id="notifications" rel="nofollow" data-target="#" href="#"
-                                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"
-                                    class="nav-link"><i class="fa fa-bell"></i><span
-                                        class="badge badge-warning">12</span></a>
-                                <ul aria-labelledby="notifications" class="dropdown-menu">
-                                    <li><a rel="nofollow" href="#" class="dropdown-item">
-                                            <div class="notification d-flex justify-content-between">
-                                                <div class="notification-content"><i class="fa fa-envelope"></i>You have
-                                                    6
-                                                    new messages </div>
-                                                <div class="notification-time"><small>4 minutes ago</small></div>
-                                            </div>
-                                        </a></li>
-                                </ul>
-                            </li>
-                            <!-- Log out-->
-                            <li class="nav-item"><a href="../logout.php" class="nav-link logout"> <span
-                                        class="d-none d-sm-inline-block">Déconnexion</span><i class="fa fa-sign-out"></i></a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-        </header>
+    <header>
+        <!-- NAVBAR -->
+        <center>
+            <img src="images/stage.jpg" class="top_img">
+        </center>
+        <nav class="navbarstyle">
+            <ul>
+            <?php if (isset($_SESSION['idpromo'])) { ?>
+                <li><a href="stage.php">Stages</a></li>
+                <li><a href="rechercheEntreprise.php">Recherche</a></li>                
+                <?php if (($_SESSION['idpromo'] == 1)) { ?>
+                <li><a href="gestioneleve.php">Gestion élèves</a></li>
+                <li><a href="tableaurec.php">Statistique</a></li>
+                <?php } else { ?>
+                <li><a href="recapitulatif.php">Récapitulatif</a></li>
+                <?php } ?>
+
+                <li class="nomCompte">  
+                    <a id="profil" href="profil.php"><?php echo $_SESSION['nom'].' '.$_SESSION['prenom'];?></a>
+                </li>
+                <li class="imageCompte">
+                <?php
+                $sqlphoto = "SELECT photo FROM sta_etudiant WHERE idetudiant = " . $_SESSION['code'];
+                $q = $connection->query($sqlphoto);
+                $ligne = $q->fetch();
+                echo '<div id="pdp">';
+                echo "<img src='images/" . $ligne['photo'] . "'>";
+                echo "</div>";
+                ?>
+                </li>
+                <li>
+                <?php
+                if (isset($_SESSION['nom'])) {
+                    echo '<a id="deco" href="logout.php">Deconnexion</a>';
+                } else {
+                    echo '<a id="co" href="log.php">Connexion</a>';
+                } ?> 
+                </li>
+            <?php } else {
+                echo '<a id="co" href="log.php">Connexion</a>';
+            } ?>                
+            </ul>
+        </nav>
+    </header>
