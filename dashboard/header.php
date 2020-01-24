58c452f7e59a540301f64c4df206a33596852df5
Guillaume Morliéras <g.morlieras@gmail.com>
2020-01-24 13:53:34 +0100

    Modification header dashboard / ajout gestion entreprises (correction)

--- dashboard/header.php
+++ dashboard/header.php
@@ -63,8 +63,8 @@ $ligne = $q->fetch();
                 <h5 class="sidenav-heading">Main</h5>
                 <ul id="side-main-menu" class="side-menu list-unstyled">
                     <li><a href="index.php"> <i class="icon-home"></i>DASHBOARD </a></li>
-                    <li><a href="index.php"> <i class="icon-home"></i>GESTION ELEVES </a></li>
-                    <li><a href="index.php"> <i class="icon-home"></i>GESTION ENTREPRISES </a></li>
+                    <li><a href="gestionEleves.php"> <i class="icon-home"></i>GESTION ELEVES </a></li>
+                    <li><a href="gestionEntreprises.php"> <i class="icon-home"></i>GESTION ENTREPRISES</a></li>
                     <li><a href="index.php"> <i class="icon-home"></i>PERIODES </a></li>
                     <li><a href="index.php"> <i class="icon-home"></i>STATS </a></li>
                 </ul>
@@ -107,7 +107,7 @@ $ligne = $q->fetch();
                             </li>
                             <!-- Log out-->
                             <li class="nav-item"><a href="../logout.php" class="nav-link logout"> <span
-                                        class="d-none d-sm-inline-block">Logout</span><i class="fa fa-sign-out"></i></a>
+                                        class="d-none d-sm-inline-block">Déconnexion</span><i class="fa fa-sign-out"></i></a>
                             </li>
                         </ul>
                     </div>
